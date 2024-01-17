50da4d5e12c1c4a47ac16e7ddbed87a935158d64
kalenduhs <kalenduhs@gmail.com>
2024-01-16 22:02:30 -0600

    Progress on Thymeleaf

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,25 +1,41 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name;
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name;
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String hello(@RequestParam String name) {
-        return "Hello, " + name;
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name;
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("form")
     public String helloForm() {
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("Kevin");
+        names.add("Brendan");
+        names.add("Nicole");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 }
